e63163e33701519b4de217d33ec68d21d3f3c712
RavingRaven4 <andy-pete@outlook.com>
2025-03-31 13:53:50 -0700

    31/03

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-// Get the hamburger menu and navbar
-const hamburgerMenu = document.getElementById("hamburgerMenu");
-const navbar = document.querySelector(".navbar");
+function setupHamburgerMenu() {
+    const hamburgerMenu = document.getElementById("hamburgerMenu");
+    const navbar = document.querySelector(".navbar");
 
-// Add click event to toggle the menu
-hamburgerMenu.addEventListener("click", () => {
-    navbar.classList.toggle("active"); // Toggle the 'active' class to show/hide the menu
-});
+    if (hamburgerMenu && navbar) {
+        hamburgerMenu.addEventListener("click", () => {
+            navbar.classList.toggle("active");
+        });
+    }
+}
